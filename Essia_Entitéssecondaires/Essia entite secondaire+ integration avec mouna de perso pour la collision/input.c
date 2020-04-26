222025104aaa7d17cd400ffd0c8b1f1d4222ac1e
Essia56 <61236323+Essia56@users.noreply.github.com>
2020-04-26 10:22:39 -0700

    Essia_entité secondaire
    Mouné et Moi,  nous avons fait l'integration de personnage et entité pour que je peux faire la collision Bounding Box

---
+++ Essia_Entitéssecondaires/Essia entite secondaire+ integration avec mouna de perso pour la collision/input.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <time.h>
+#include <unistd.h>
+#include "input.h"
+int direction(SDL_Event event,int *continuer){
+int d;
+SDL_PollEvent(&event);
+switch(event.type)
+    {
+        case SDL_QUIT:
+            *continuer = 0;
+  		break;
+	case SDL_KEYDOWN:
+
+            switch (event.key.keysym.sym)
+            {
+ 		case SDLK_ESCAPE:
+		    continuer=0 ;
+			break ;
+
+		case SDLK_LEFT :
+		d=2;	
+                    	break ;
+
+		case SDLK_RIGHT :
+		d=1; 
+			break ;
+
+		case SDLK_UP :
+		d=3;	
+                    	break ;
+
+		case SDLK_DOWN :
+		d=4; 
+			break ;
+	    }
+		break;
+    }
+return d;
+}
