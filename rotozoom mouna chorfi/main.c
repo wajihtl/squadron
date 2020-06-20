8dd0e980ba4498ae4ca010f136377965b3e62071
mouna10 <61240937+mouna10@users.noreply.github.com>
2020-06-19 19:32:25 -0700

    Add files via upload

---
+++ rotozoom mouna chorfi/main.c
@@ -0,0 +1,28 @@
+/**
+* @file main.c
+* @brief Integrated Program.
+* @author Chorfi mouna
+* @version 0.1
+* @date juin 01, 2020
+*
+* Integrated program for rotozoom
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_rotozoom.h>
+#include "rotozoom.h"
+
+
+int main(int argc, char *argv[])
+{
+
+ rotozoom();
+
+
+ SDL_Quit();
+
+return 0;
+}
+
